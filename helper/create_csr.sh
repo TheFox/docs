dcbf40f3b5b53353098ba3a8b27f833adde9a5de
Christian Mayer <christian@fox21.at>
2019-09-20 07:47:51 +0200

    Separate helper scripts.
    Move to TheFox/helper@f69d44c58d6a04429bd0869572c616471ed21497

--- helper/create_csr.sh
+++
@@ -1,4 +0,0 @@
-
-openssl req -new -newkey rsa:4096 -nodes -out projectname.csr -keyout projectname.key
-openssl req -new -key projectname.key -out projectname_20181207.csr
-openssl req -noout -text -in projectname.csr
--- helper/graylog.sh
+++
@@ -1,6 +0,0 @@
-# Run Graylog in Docker
-# http://docs.graylog.org/en/stable/pages/installation/docker.html
-
-docker run -d --name mongo --restart always mongo:3
-docker run -d --name elasticsearch -e "discovery.type=single-node" -e "bootstrap.memory_lock=false" -e "http.host=0.0.0.0" -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" --restart always docker.elastic.co/elasticsearch/elasticsearch:6.6.1
-docker run -d --name graylog --link mongo --link elasticsearch -p 9000:9000 -p 12201:12201 -p 1514:1514 -p 5555:5555 -e GRAYLOG_HTTP_EXTERNAL_URI="http://127.0.0.1:9000/" -e GRAYLOG_ROOT_PASSWORD_SHA2=8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918 --restart always graylog/graylog:3.0
